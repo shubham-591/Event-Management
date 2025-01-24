7ddce6f6295768e580572748ccefe262808c456c
shubham-591 <130385835+shubham-591@users.noreply.github.com>
2025-01-24 11:23:58 +0530

    Update db.js file and added necessary changes

--- config/db.js
+++ config/db.js
@@ -7,19 +7,12 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    // 'event_management', // Database name
-    // 'postgres',         // Database user
-    // 'Shubh@m123',       // Database password
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false,
         
 });
-// console.log('Database Host:', process.env.DB_HOST);
-// console.log('Database Name:', process.env.DB_NAME);
-// console.log('Database User:', process.env.DB_USER);
-// console.log('Database Password:', process.env.DB_PASSWORD);
 
 
 export default sequelize;
